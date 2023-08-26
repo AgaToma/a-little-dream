b1563400548eab011f1a9c04479bd400ab3b53cd
AgaToma <aga.tomashevska@gmail.com>
2023-08-26 20:09:22 +0000

    add allauth links to navbavr

--- a_little_dream/urls.py
+++ a_little_dream/urls.py
@@ -20,6 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('djrichtextfield', include('djrichtextfield.urls')),
     path('', include('home.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
