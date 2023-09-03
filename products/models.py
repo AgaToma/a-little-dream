20d45965d5d492b13e9864c75adeea4144b1e8c8
AgaToma <aga.tomashevska@gmail.com>
2023-09-03 12:35:39 +0000

    add ages.json

--- products/models.py
+++ products/models.py
@@ -43,9 +43,13 @@ class TargetAge(models.Model):
     """Model to be referenced for multiple selection
     of product target age groups"""
     name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
 
 
 class Product(models.Model):
