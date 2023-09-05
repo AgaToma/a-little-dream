cc28f456be527cc12cf55d536cfd2a6ab61b2531
AgaToma <aga.tomashevska@gmail.com>
2023-09-05 16:58:58 +0000

    register target age in admin

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, TargetAge
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -30,4 +30,4 @@ class TargetAgeAdmin(admin.ModelAdmin):
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
-
+admin.site.register(TargetAge, TargetAgeAdmin)
