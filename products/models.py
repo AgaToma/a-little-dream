2876850974ed0de95bc17ec6386dccbfd4cfdf0f
AgaToma <aga.tomashevska@gmail.com>
2023-09-05 16:45:34 +0000

    fix: typos in products, number of digits in price, load products fixture

--- products/models.py
+++ products/models.py
@@ -62,7 +62,7 @@ class Product(models.Model):
     author = models.CharField(max_length=200)
     highlights = RichTextField(max_length=500, null=False, blank=False)
     description = RichTextField(max_length=10000, null=False, blank=False)
-    price = models.DecimalField(max_digits=3, decimal_places=2)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = ResizedImageField(
         size=[400, None],
