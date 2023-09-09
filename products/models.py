d0b8a8a82c8290083b3c640d16a061f13e4efcd3
AgaToma <aga.tomashevska@gmail.com>
2023-09-09 11:05:43 +0000

    fix upload path for product photo

--- products/models.py
+++ products/models.py
@@ -67,7 +67,7 @@ class Product(models.Model):
     image = ResizedImageField(
         size=[400, None],
         quality=75,
-        upload_to="rooms/",
+        upload_to="products/",
         force_format="WEBP",
         blank=False,
         null=False,
