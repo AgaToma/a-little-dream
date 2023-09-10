ff24cb72d7741a58af68d2a09378df98818bccf2
AgaToma <aga.tomashevska@gmail.com>
2023-09-10 12:44:42 +0000

    add contexts.py with basic bag logic

--- bag/views.py
+++ bag/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from decimal import Decimal
+from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 # Create your views here.
