f06b41ec6cd10c14537132314375d41d94434d6a
AgaToma <aga.tomashevska@gmail.com>
2023-09-10 16:02:24 +0000

    complete bag contents logic

--- bag/contexts.py
+++ bag/contexts.py
@@ -22,3 +22,25 @@ def bag_contents(request):
             "product": product,
             "product_count": product_count,
         })
+
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = Decimal(settings.STANDARD_DELIVERY)
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    else:
+        delivery = 0
+        free_delivery_delta = 0
+
+    grand_total = delivery + total
+
+    context = {
+        'bag_items': bag_items,
+        'total': total,
+        'product_count': product_count,
+        'delivery': delivery,
+        'free_delivery_delta': free_delivery_delta,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+        'grand_total': grand_total,
+        "user": user,
+    }
+
+    return context
