7e14afb3f2b2cc4c38c5fb6e98790b93790fe44f
AgaToma <aga.tomashevska@gmail.com>
2023-09-28 08:03:34 +0000

    remove whitenoise

--- a_little_dream/settings.py
+++ a_little_dream/settings.py
@@ -83,7 +83,6 @@ DJRICHTEXTFIELD_CONFIG = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
