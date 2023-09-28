973e6b3c20419d6302615fcb869d31fb64c34d9b
AgaToma <aga.tomashevska@gmail.com>
2023-09-28 16:11:47 +0000

    remove blanklines

--- products/forms.py
+++ products/forms.py
@@ -8,12 +8,12 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
-    
+
     target_ages = forms.ModelMultipleChoiceField(
         queryset=TargetAge.objects.all(),
         widget=forms.CheckboxSelectMultiple)
     author = forms.CharField(required=False)
-      
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
