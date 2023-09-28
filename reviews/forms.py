bafdeca0fc003a15199628ca723cb0df922f39a3
AgaToma <aga.tomashevska@gmail.com>
2023-09-28 11:11:23 +0000

    add review form

---
+++ reviews/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from djrichtextfield.widgets import RichTextWidget
+from .models import Review
+
+
+class ReviewForm(forms.ModelForm):
+    """
+    Form to create a product review
+    """
+    model = Review
+    fields = [
+        'rating',
+        'content'
+    ]
+
+    content = forms.CharField(widget=RichTextWidget())
+
+    labels = {
+        'rating': 'Rating (1-5)',
+        'content': 'Content',
+    }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.fields['rating'].widget.attrs['min'] = 1
+        self.fields['rating'].widget.attrs['max'] = 5
