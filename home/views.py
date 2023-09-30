b22de8c574ca585795a5b2fee3d2432c115a36f6
AgaToma <aga.tomashevska@gmail.com>
2023-09-30 19:35:55 +0000

    add privacy and terms

--- home/views.py
+++ home/views.py
@@ -7,3 +7,13 @@ def index(request):
     """
     context = {}
     return render(request, "home/index.html", context)
+
+
+def privacy(request):
+    """A view to return the privacy policy page"""
+    return render(request, "home/privacy.html")
+
+
+def terms(request):
+    """A view to return the terms and conditions page"""
+    return render(request, "home/terms.html")
