be2d0f7fb838d5c2d926f4ed57cbddf777be1242
AgaToma <aga.tomashevska@gmail.com>
2023-10-01 21:17:45 +0000

    add error pages, move robots.txt

--- a_little_dream/urls.py
+++ a_little_dream/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,5 +30,14 @@ urlpatterns = [
     path('profiles/', include('profiles.urls')),
     path('reviews/', include('reviews.urls')),
     path('stories/', include('stories.urls')),
+    path(
+        'robots.txt',
+        TemplateView.as_view(
+            template_name='robots.txt', content_type='text/plain'
+        ),
+    ),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler403 = "a_little_dream.views.error_403"
+handler404 = "a_little_dream.views.error_404"
